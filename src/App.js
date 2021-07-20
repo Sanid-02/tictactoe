1497e63d685d3b275bfdc3b23863e752345bf6df
sanid <sanid.2001@gmail.com>
2021-07-20 13:43:49 +0530

    displayed draw message with conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Board from "./components/Board";
 import History from "./components/History";
+import StatusMessage from "./components/StatusMessage";
 import { calculateWinner } from "./helpers";
 
 import "./styles/root.scss";
@@ -18,9 +19,6 @@ const App = () => {
 
   const winner = calculateWinner(current.board);
   // console.log(winner);
-  const message = winner
-    ? `Winner is ${winner}`
-    : `Next Player is ${current.isXNext ? "X" : "O"}`;
 
   const handleSquareClick = (position) => {
     if (current.board[position] || winner) {
@@ -52,7 +50,8 @@ const App = () => {
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
-      <h2>{message}</h2>
+      <StatusMessage winner={winner} current={current} />
+      {/* <h2>{message}</h2> */}
       <Board board={current.board} handleSquareClick={handleSquareClick} />
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
---
+++ src/components/StatusMessage.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const StatusMessage = ({ winner, current }) => {
+  const noMovesLeft = current.board.every((el) => el !== null); //every is predefined function
+  return (
+    // const message = winner
+    // ? `Winner is ${winner}`
+    // : `Next Player is ${current.isXNext ? "X" : "O"}`;
+
+    // {winner && `Winner is ${winner}`} => Always retun Boolean values.
+
+    <h2>
+      {winner && `Winner is ${winner}`}
+      {!winner &&
+        !noMovesLeft &&
+        `Next Player is ${current.isXNext ? "X" : "O"}`}
+      {!winner && noMovesLeft && `X and O tied`}
+    </h2>
+  );
+};
+
+export default StatusMessage;
